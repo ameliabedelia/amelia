0331283c9fb137f31c8f0ad87ad86745895a1bdf
ameliabedelia <amelia.barber@gmail.com>
2021-02-09 12:36:50 +0100

    add geometric mean function

--- R/stat.R
+++ R/stat.R
@@ -13,3 +13,20 @@
 se <- function(x, na.rm = TRUE){
      sd(x, na.rm = na.rm)/sqrt(length(x[na.omit(x)]))
 }
+
+
+#' Geometric mean
+#'
+#' This function calculates the geometric mean of the numeric vector `x`.
+#' @keywords geometric mean
+#'
+#' @param x a numeric vector
+#' @param na.rm logical. Whether missing values should be removed.
+#' @export
+#' @examples
+#'
+#'
+#'
+gm_mean = function(x, na.rm=TRUE){
+     exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
